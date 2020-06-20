fb5be9bb8d58fe253e2c29afd8a840b0fe66c312
shukur-alom <62952159+shukur-alom@users.noreply.github.com>
2020-06-20 09:20:11 -0700

    Add files via upload

---
+++ login cl.py
@@ -0,0 +1,34 @@
+import getpass,datetime
+t=datetime.datetime.now()
+class Login:
+   def __init__(self,aemail,apass):
+      self.email=aemail
+      self.pas=apass
+   def shu(self):
+      rr=getpass.getpass('pass\n')
+      sh=open('shukur_login_info.txt','a')
+      if User_inp==shukur.email and rr==self.pas:
+         sh.write(F"your last login,Date:{t.date()}, Time:{t.time()}")
+         sh.close()
+         print('Allah')
+      elif User_inp==alam.email and rr==self.pas:
+         sh.write(F"your last login,Date:{t.date()}, Time:{t.time()}")
+         sh.close()
+         print(' I love Allah')
+      else:
+         sh.close()
+   @classmethod
+   def sty(cls,strt):
+      return cls(*strt.split('-'))
+
+shukur=Login.sty('shukuralom1234@gmail.com-shukur')
+alam=Login.sty('scoutsnb@gmail.com-alam')
+
+User_inp=input('email\n')
+
+if  User_inp == shukur.email:
+   shukur.shu()
+elif User_inp == alam.email:
+   alam.shu()
+else:
+   pass
